be1336449d78a5b4a438491970496c1024893069
Freek Wielstra <fr.wielstra@gmail.com>
2022-12-04 12:40:11 +0100

    Clean up day 3 implementations

--- day3/day3.go
+++ day3/day3.go
@@ -2,32 +2,16 @@ package day3
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"strings"
 	"unicode"
 )
-
-// TODO: merge findDiff and findDiffs
-// assumes there's only ever one character that is different
-func findDiff(s1 string, s2 string) rune {
-	for _, c := range s1 {
-		if strings.ContainsRune(s2, c) {
-			return c
-
-		}
-	}
-
-	fmt.Printf("cannont find diff character in %s and %s", s1, s2)
-	return 0
-}
 
 func findDiffs(s1 string, s2 string) string {
 	result := ""
 	for _, c := range s1 {
 		if strings.ContainsRune(s2, c) {
 			result += string(c)
-
 		}
 	}
 
@@ -52,8 +36,8 @@ func SumCompartiments(input io.Reader) int {
 		length := len(line)
 		left := line[0 : length/2]
 		right := line[(length / 2):length]
-		diff := findDiff(left, right)
-		sum += priority(diff)
+		diff := findDiffs(left, right)
+		sum += priority(rune(diff[0]))
 	}
 
 	return sum
@@ -63,22 +47,17 @@ func findBadge(buffer [3]string) rune {
 	first := findDiffs(buffer[0], buffer[1])
 	second := findDiffs(first, buffer[2])
 	// note: result may contain multiple instances of the matching character. This is fine.
-
 	return rune(second[0])
 }
-
-// TODO: clean up
 
 func SumBadges(input io.Reader) int {
 	scanner := bufio.NewScanner(input)
 	sum := 0
 	buffer := [3]string{}
 	i := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		if line == "" {
-			continue
-		}
 		buffer[i] = line
 		i++
 		if i == 3 {
