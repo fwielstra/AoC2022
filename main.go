a223137d852e5925a481cda8761fa760ad889128
Freek Wielstra <fr.wielstra@gmail.com>
2022-12-04 14:05:01 +0100

    Refactor repetitive file loading and timing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fwielstra/AoC2022/day1"
 	"github.com/fwielstra/AoC2022/day2"
 	"github.com/fwielstra/AoC2022/day3"
+	"io"
 	"os"
 	"strconv"
 	"time"
@@ -12,97 +13,84 @@ import (
 
 func main() {
 	day := os.Args[1]
-	args := os.Args[2:]
+	filename := os.Args[2]
+	args := os.Args[3:]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("error opening file %s: %s\n", filename, err)
+		os.Exit(1)
+	}
 
 	switch day {
 	case "day1":
-		runDay1(args)
+		runDay1(filename, args)
 	case "day2":
-		runDay2(args)
+		runDay2(filename)
 	case "day3":
-		runDay3(args)
+		runDay3(filename)
 	default:
 		fmt.Printf("unrecognized day %s\n", day)
 	}
+
+	file.Close()
 }
 
-func runDay1(args []string) {
-	if len(args) != 2 {
-		fmt.Printf("invalid args")
-		os.Exit(1)
-	}
-
-	filename := args[0]
-	top, _ := strconv.Atoi(args[1])
-
+func withFile(filename string, callback func(r io.Reader)) {
 	file, err := os.Open(filename)
-	defer file.Close()
-
 	if err != nil {
-		fmt.Printf("error opening input file %s: %v", filename, err)
+		fmt.Printf("error opening file %s: %s\n", filename, err)
 		os.Exit(1)
 	}
+	start := time.Now()
+	callback(file)
+	elapsed := time.Since(start)
+	fmt.Printf("processing file %s took %s\n", filename, elapsed)
 
-	result := day1.FindHighestCalories(file, top)
-	fmt.Printf("Day 1 result top %d for file %s: %d\n", top, filename, result)
+	err = file.Close()
+	if err != nil {
+		fmt.Printf("error closing file %s: %s\n", filename, err)
+		os.Exit(1)
+	}
 }
 
-func runDay2(args []string) {
-	if len(args) != 1 {
-		fmt.Printf("invalid args")
-		os.Exit(1)
-	}
-
-	filename := args[0]
-	file, err := os.Open(filename)
-	defer file.Close()
-
-	if err != nil {
-		fmt.Printf("error opening input file %s: %v", filename, err)
-		os.Exit(1)
-	}
-
-	start := time.Now()
-	scores := day2.GetScores(file)
-	sum := day2.Sum(scores)
-	elapsed := time.Since(start)
-	fmt.Printf("Result for day 2: %d in %s\n", sum, elapsed)
-
-	file.Close()
-
-	// simple approach
-	file, _ = os.Open(filename)
-	start = time.Now()
-	dumb := day2.Dumb(file)
-	elapsed = time.Since(start)
-	fmt.Printf("Result for day 2 dumb: %d in %s\n", dumb, elapsed)
-
-	// part 2
-
-	file, _ = os.Open(filename)
-	start = time.Now()
-	scores = day2.GetScoresPartTwo(file)
-	sum = day2.Sum(scores)
-	elapsed = time.Since(start)
-	fmt.Printf("Result for day 2 part 2: %d in %s\n", sum, elapsed)
-
-	file.Close()
+func runDay1(filename string, args []string) {
+	withFile(filename, func(r io.Reader) {
+		top, _ := strconv.Atoi(args[0])
+		result := day1.FindHighestCalories(r, top)
+		fmt.Printf("Day 1 result top %d for file %s: %d\n", top, filename, result)
+	})
 }
 
-func runDay3(args []string) {
-	filename := args[0]
-	file, _ := os.Open(filename)
+func runDay2(filename string) {
+	withFile(filename, func(r io.Reader) {
+		scores := day2.GetScores(r)
+		sum := day2.Sum(scores)
+		fmt.Printf("Result for day 2: %d\n", sum)
+	})
 
-	start := time.Now()
-	sum := day3.SumCompartiments(file)
-	elapsed := time.Since(start)
-	fmt.Printf("Result for day 1 part 1: %d in %s\n", sum, elapsed)
-	file.Close()
+	withFile(filename, func(r io.Reader) {
+		// simple approach
+		dumb := day2.Dumb(r)
+		fmt.Printf("Result for day 2 dumb: %d\n", dumb)
+	})
 
-	file, _ = os.Open(filename)
-	start = time.Now()
-	sum = day3.SumBadges(file)
-	elapsed = time.Since(start)
-	fmt.Printf("Result for day 1 part 2: %d in %s\n", sum, elapsed)
-	file.Close()
+	withFile(filename, func(r io.Reader) {
+		// part 2
+		scores := day2.GetScoresPartTwo(r)
+		sum := day2.Sum(scores)
+		fmt.Printf("Result for day 2 part 2: %d\n", sum)
+	})
+}
+
+func runDay3(filename string) {
+	withFile(filename, func(r io.Reader) {
+		sum := day3.SumCompartiments(r)
+		fmt.Printf("Result for day 1 part 1: %d\n", sum)
+	})
+
+	withFile(filename, func(r io.Reader) {
+		sum := day3.SumBadges(r)
+		fmt.Printf("Result for day 1 part 2: %d\n", sum)
+	})
 }
